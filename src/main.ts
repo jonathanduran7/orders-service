93223784d9e55c46b26adc492234f12ce593a187
jonathanduran7 <joniduran624@gmail.com>
2025-04-19 20:58:42 -0300

    refactor main.ts to create a microservice using RabbitMQ for order processing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://rabbit:5672'],
+        queue: 'orders_queue',
+        queueOptions: { durable: true },
+      },
+    },
+  );
+  await app.listen();
+  console.log('🟢 orders‑service listo');
 }
 bootstrap();
