37aeb5110fe25e94793a24f5551c7a5a029394d4
jonathanduran7 <joniduran624@gmail.com>
2025-04-24 20:21:45 -0300

    Add quantity and status fields to Order entity with default values

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -7,4 +7,6 @@ export class Order {
   @Column() productId: string;
   @Column('numeric', { precision: 10, scale: 2 }) total: number;
   @CreateDateColumn() createdAt: Date;
+  @Column({ default: 1 }) quantity: number;
+  @Column({ default: 'pending' }) status: string;
 }
