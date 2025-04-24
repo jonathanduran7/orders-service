cf5979fc1cdebb054e70fc0158cb139afa3bd0f5
jonathanduran7 <joniduran624@gmail.com>
2025-04-24 20:22:14 -0300

    Integrate RabbitMQ client into OrdersModule for message handling, enabling communication with the products queue.

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,9 +3,23 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
+import { ClientsModule } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [
+    TypeOrmModule.forFeature([Order]),
+    ClientsModule.register([
+      {
+        name: 'ORDERS_PUB',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://admin:secret123@rabbit:5672'],
+          queue: 'products_queue'
+        },
+      },  
+    ]),
+  ],
   providers: [OrdersService],
   controllers: [OrdersController],
 })
